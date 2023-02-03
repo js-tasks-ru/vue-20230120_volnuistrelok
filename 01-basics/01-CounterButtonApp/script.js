6866d650b22709b70233511d2021b5ef63500755
Ann Mindorina <volnuistrelok@gmail.com>
2023-02-03 17:31:00 +0300

    button_click

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,13 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const App = {
+  name: 'App',
+  data: function () {
+    return { counter: 0 };
+  },
+};
+const app = createApp(App);
+const vm = app.mount('#app');
+window.vm = vm;
